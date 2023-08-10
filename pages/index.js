18a215f83f5872ec0fe9a3bad2e59bebb9c702d2
Kinshuk Goel <shuknuk@kinshuks-air.mynetworksettings.com>
2023-08-10 00:05:19 -0400

    changes made

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+    
+    return (
+        <div>
+            <h1>Home</h1>
+
+            <img src="https://cdn.vox-cdn.com/thumbor/Si2spWe-6jYnWh8roDPVRV7izC4=/0x0:1192x795/1400x788/filters:focal(596x398:597x399)/cdn.vox-cdn.com/uploads/chorus_asset/file/22312759/rickroll_4k.jpg"></img>
+        </div>
+    );
+
+
+}
