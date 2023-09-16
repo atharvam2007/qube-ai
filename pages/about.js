6c23a5096ef5f48bb2586ed69361b52c77e0dc51
atharvam2007 <141259883+atharvam2007@users.noreply.github.com>
2023-09-15 22:36:43 -0400

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import styles from './index.module.scss';
+import Navbar from '../comps/Navbar';
+import dynamic from 'next/dynamic';
+
+const AboutPage = () => {
+
+return(
+   <p> hi </p> 
+);
+}
