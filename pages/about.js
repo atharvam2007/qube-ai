139bedab147b89d5cd7bbcbe0ad26f13987640d1
atharvam2007 <141259883+atharvam2007@users.noreply.github.com>
2023-09-15 22:40:25 -0400

    about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styles from './index.module.scss';
 import Navbar from '../comps/Navbar';
-import dynamic from 'next/dynamic';
+//import dynamic from 'next/dynamic';
 
 const AboutPage = () => {
 
